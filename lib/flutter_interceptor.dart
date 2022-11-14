a181dfd06d554bb4ad5430564dee00ff86e4a13e
cy <seongbrave@sina.com>
2022-11-14 17:25:44 +0800

    public simple_interceptor

--- lib/flutter_interceptor.dart
+++ lib/flutter_interceptor.dart
@@ -1,5 +1,7 @@
 library flutter_interceptor;
 
-export 'package:flutter_interceptor/src/ui/interceptor_draggable.dart';
 export 'package:flutter_interceptor/src/interceptor/ui_net_interceptor.dart';
 export 'package:flutter_interceptor/src/interceptor_localizations.dart';
+export 'package:flutter_interceptor/src/ui/interceptor_detail.dart';
+export 'package:flutter_interceptor/src/ui/interceptor_draggable.dart';
+export 'package:flutter_interceptor/src/ui/simple_interceptor.dart';
